475766af077dbe796a6d1bfa4595f84ee5b0715d
Kenneth Huang <kthuang09@gmail.com>
2026-03-25 05:43:37 -0400

    fix: resolve empty test suite errors failing CI

--- src/tools/dns-records-lookup/dns-records-lookup.service.test.ts
+++
@@ -1,5 +0,0 @@
-// import { } from './dns-records-lookup.service';
-//
-// describe('dns-records-lookup', () => {
-//
-// })
--- src/tools/x509-certificate-decoder/x509-certificate-decoder.service.test.ts
+++ src/tools/x509-certificate-decoder/x509-certificate-decoder.service.test.ts
@@ -1,5 +1,8 @@
-// import { } from './x509-certificate-decoder.service';
-//
-// describe('x509-certificate-decoder', () => {
-//
-// })
+import { describe, expect, it } from 'vitest';
+import { decodeCert } from './x509-certificate-decoder.service';
+
+describe('x509-certificate-decoder', () => {
+  it('should be able to export decodeCert', () => {
+    expect(decodeCert).toBeDefined();
+  });
+});
